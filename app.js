999e4eb568357ab5acaefd428de82b78ce21d00e
Jonathan <jonathan@thephpsolution.com>
2013-02-09 12:04:02 -0600

    Committing changes before dropping in new design.

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-Ext.application({
-    requires: ['Ext.container.Viewport'],
-    name: 'MF',
+var MOUSE_PER_VIDEO = 4;
 
-    appFolder: 'app/MouseFloat',
-    
-    controllers: [
-        'Tests'
-    ],
+$(document).ready(function() {
+    _V_('fst-video').ready(function(){
+        var floatLogs = [];
+        for (var i = 0; i < MOUSE_PER_VIDEO; i++) {
+            floatLogs.push(new MF.model.FloatLog(i, _V_('fst-video').duration()));
 
-    launch: function() {
-//        Ext.create('Ext.container.Viewport', {
-//            layout: 'fit',
-//            items: [
-//                {
-//                    xtype: 'fstwizard'
-//                }
-//            ]
-//        });
-        
-        this.getController('Tests').wizardAction();
-    }
+            var details = $(
+                '<div class="floatlog-summary" id="floatlog-' + i + '">'
+                    + '<div class="heading">(...)</div>'
+                    + '<table>'
+                        + '<thead><th>Interval</th><th>Float Time</th></thead>'
+                        + '<tbody>'
+                            + '<tr><th>:00 - :02</th><td id="f'+i+'_ival00-02">...</td></tr>'
+                            + '<tr><th>:02 - :04</th><td id="f'+i+'_ival02-04">...</td></tr>'
+                            + '<tr><th>:04 - :06</th><td id="f'+i+'_ival04-06">...</td></tr>'
+                            + '<tr><th>:00 - :06</th><td id="f'+i+'_ival00-06">...</td></tr>'
+                        + '</tbody>'
+                    + '</table>'
+                + '</div>'
+            );
+
+            details.appendTo($('#test-details'));
+        }
+
+        $('#video-hijack').click(function(e) {
+            var index = Math.floor( e.pageX / $(this).width() / .25);
+            var video = _V_('fst-video');
+
+            floatLogs[index].toggle(video.currentTime());
+            console.log(floatLogs[index].toggles);
+
+            var div = $('<div style="background-color: white;"></div>');
+            div.hide();
+            div.css('width', $(this).width() / 4);
+            div.css('height', $(this).height());
+            div.css('position', 'absolute');
+            div.css('left', index * $(this).width() / 4);
+
+            $(this).append(div);
+            div.fadeTo(100, 0.5).fadeOut(100);
+        });
+    });
 });
--- app/MouseFloat/model/FloatLog.js
+++ app/MouseFloat/model/FloatLog.js
@@ -1,14 +1,54 @@
-Ext.ns('MF.model');
+if (typeof MF == "undefined") {
+    MF = {
+        model: {}
+    };
+}
 
-Ext.define('MF.model.FloatLog', 
-            MF.model.FloatLog = {
-    extend: 'Ext.data.Model',
-    hasOne: 'MF.model.Mouse',
-    fields: [
-        {name: 'id',        type: 'int'},
-        {name: 'mouse_id',   type: 'string'}
-    ],
-    getMouse: function() {
-        return new MF.model.Mouse();
-    }
-});
+MF.model.FloatLog = function(id, duration) {
+    this.toggles = [];
+    this.state = 'Floating';
+    
+    this.getStatus = function() {
+        return this.state;
+    };
+    
+    this.sumFloat = function(index) {
+        var sum = 0.0;
+        var floating = true;
+        for (var i = 0; i < this.toggles.length; i++) {
+            if ((i % 2) * floating) {
+                if (i == this.toggles.length - 1) {
+                    sum += _V_('fst-video').currentTime() - this.toggles[i].location;
+                } else {
+                    sum += this.toggles[i + 1].location - this.toggles[i].location;
+                }
+            }
+        }
+        
+        return sum;
+    };
+    
+    this.init = function(id, duration) {
+        console.log(duration);
+        this.id = id;
+        this.duration = duration;
+    };
+    
+    this.toggle = function(location) {
+        this.toggles.push({
+            location: location
+        });
+        
+        this.state = this.state == 'Floating' ? 'Swimming' : 'Floating';
+        this.updateView();
+    };
+    
+    this.updateView = function() {
+        var summary = $('#floatlog-' + this.id);
+        $('.heading', summary).text(this.getStatus());
+        
+        $('#f'+this.id+'_ival00-02', summary).text(this.sumFloat(0));
+    };
+    
+    this.init(id, duration);
+};
